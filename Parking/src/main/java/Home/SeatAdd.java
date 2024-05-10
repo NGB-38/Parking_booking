aaa7558b506c255aef16be45eb3937a3d6c195cc
Nhung55555 <itdsiu22167@student.hcmiu.edu.vn>
2024-05-10 15:27:28 +0700

    adding

--- Parking/src/main/java/Home/SeatAdd.java
+++ Parking/src/main/java/Home/SeatAdd.java
@@ -3,6 +3,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package Home;
+import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 
 /**
  *
@@ -15,7 +21,33 @@ public class SeatAdd extends javax.swing.JFrame {
      */
     public SeatAdd() {
         initComponents();
+        Connect();
     }
+    Connection con;
+    PreparedStatement pst;
+    
+   
+    
+    public void Connect(){
+        //connect vô database của mình, nguồn là mysql còn db tên là carregis (db phải in đậm mới xài được)
+      
+        String url="jdbc:mysql://localhost:3306/carregis";
+        String user="root";
+        String password="12345";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(url, user, password);
+            } 
+            catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            }
+        
+    }
+    
+    
+    
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -30,8 +62,8 @@ public class SeatAdd extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
-        jComboBox1 = new javax.swing.JComboBox<>();
-        jDateChooser1 = new com.toedter.calendar.JDateChooser();
+        txtpark = new javax.swing.JComboBox<>();
+        txtdate = new com.toedter.calendar.JDateChooser();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
 
@@ -62,7 +94,7 @@ public class SeatAdd extends javax.swing.JFrame {
 
         jLabel3.setText("Date");
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        txtpark.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "1111", " " }));
 
         jButton1.setText("Cancel");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -72,6 +104,11 @@ public class SeatAdd extends javax.swing.JFrame {
         });
 
         jButton2.setText("Add");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -85,8 +122,8 @@ public class SeatAdd extends javax.swing.JFrame {
                     .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jDateChooser1, javax.swing.GroupLayout.DEFAULT_SIZE, 112, Short.MAX_VALUE)
-                    .addComponent(jComboBox1, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(txtdate, javax.swing.GroupLayout.DEFAULT_SIZE, 112, Short.MAX_VALUE)
+                    .addComponent(txtpark, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(79, 79, 79))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGap(56, 56, 56)
@@ -102,10 +139,10 @@ public class SeatAdd extends javax.swing.JFrame {
                 .addGap(23, 23, 23)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
-                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(txtpark, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(27, 27, 27)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jDateChooser1, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtdate, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(11, 11, 11)
                         .addComponent(jLabel3)))
@@ -124,6 +161,35 @@ public class SeatAdd extends javax.swing.JFrame {
         // TODO add your handling code here:
         this.setVisible(false);
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        String parkno = txtpark.getSelectedItem().toString();
+        SimpleDateFormat Date_Format = new SimpleDateFormat("yyyy-MM-dd");
+        String date = Date_Format.format(txtdate.getDate());
+        
+        
+       for(int i=1; i<=20; i++)
+       {
+           int seats = i;
+           String status = "unbooked";
+           
+            try {
+                pst = con.prepareStatement ("insert into seat(parkno, seats,date, status)values(?,?,?,?)");
+                pst.setString(1,parkno);
+                pst.setInt(2,seats);
+                pst.setString(3,date);
+                pst.setString(4, status);
+                pst.executeUpdate();
+
+            } catch (SQLException ex) {
+                Logger.getLogger(SeatAdd.class.getName()).log(Level.SEVERE, null, ex);
+            }
+       }
+       JOptionPane.showMessageDialog(this,"Seat Added");
+       
+       
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -151,6 +217,27 @@ public class SeatAdd extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(SeatAdd.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+//        String url="jdbc:mysql://localhost:3306/mysql";
+//        String user="root";
+//        String password="12345";
+//        try {
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//            Connection connection = DriverManager.getConnection(url, user, password);
+//            System.out.println("Connection is Successful to the database"+url);
+////            String query = "insert into customer(id, vehicle_number, registration_date,contact_number) values(1,'123','2022-2-2','0000000000')";
+////            Statement statement = connection.createStatement(query);
+////            statement.execute(query);
+////        
+////        
+////        
+//            } 
+//            catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+//            } catch (SQLException throwables) {
+//            throwables.printStackTrace();
+//            }
+//            }
+//            }
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -163,11 +250,11 @@ public class SeatAdd extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
-    private javax.swing.JComboBox<String> jComboBox1;
-    private com.toedter.calendar.JDateChooser jDateChooser1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
+    private com.toedter.calendar.JDateChooser txtdate;
+    private javax.swing.JComboBox<String> txtpark;
     // End of variables declaration//GEN-END:variables
 }
